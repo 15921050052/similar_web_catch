b9242cf2f463d6309069a5b500c00e38f1f6f31f
匿名者475 <1059876295@qq.com>
2018-03-08 14:15:47 +0800

    增加测试的路径

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
-
+# https://api.hitbtc.com/api/2/public/trades/ETHBTC
 url = "https://poloniex.com/public?command=returnTradeHistory&start=1520294400&end=1520295050&currencyPair=BTC_NXT"
 
 response = requests.request("GET", url)
